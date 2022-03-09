2f2b9bff6ae0bcd5d511318cc386f8782536b361
Spoochy <jordeytje@live.nl>
2022-03-09 20:35:46 +0000

    update help text

--- commands/help.js
+++ commands/help.js
@@ -8,9 +8,12 @@ module.exports = {
 		  .setColor('#0099ff')
 		  .setTitle('Chippie music Help')
 		  .setDescription(
-		  	'Onderstaande commando\'s worden uitgevoerd met **een streepje** als **prefix -**\n' +
-		  	'De blokhaken zoals [deze] staan voor argumenten, deze haken moet je niet typen.\n\n' +
-		  	'Je kan gebruik maken van **play** | **p**, **skip** | **s**, **queue** | **q** of **stop**.')
+        '**play** of **p** om je nummertjes af te spelen \n' +
+        '**skip** of **s** om je nummertjes te skippen \n' +
+        '**queue** of **q** om je nummertjes te skippen \n' +
+        '**stop** nummertje te stoppen \n' +
+        '**help** makker wat denk je zelf \n\n' +
+        '**Als de bot em tript** gebruik een paar keer **skip** en dan **play** zonder argumenten. (dit triggert *The Suprise* en fixt de bot).')
 		  .setFooter('Heb je een idee of toevoeging voor de bot? Let me know.');
 
 		  message.channel.send(embed);
